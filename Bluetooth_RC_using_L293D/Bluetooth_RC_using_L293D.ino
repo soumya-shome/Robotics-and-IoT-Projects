73d0aa6b10916672f30216bab0c7a478a524552c
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2020-10-31 13:48:40 +0530

    Uploading version_1.0
    version_1.0 have 2 variations. One using the general L293D module, another using the Arduino Motor Shield.

---
+++ Bluetooth_RC_using_L293D/Bluetooth_RC_using_L293D.ino
@@ -0,0 +1,80 @@
+#include<SoftwareSerial.h>
+int motorPin1  = 12;
+int motorPin2  = 13;
+int motorPin3  = 6;
+int motorPin4  = 5;
+
+int bluetoothTx = A1; 
+int bluetoothRx = A0;
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+String readString;
+
+void setup(){
+   pinMode(motorPin1, OUTPUT);
+   pinMode(motorPin2, OUTPUT);
+   pinMode(motorPin3, OUTPUT);
+   pinMode(motorPin4, OUTPUT);
+   Serial.begin(9600);
+   bluetooth.begin(9600);
+}
+
+void loop(){
+  while(bluetooth.available()){
+    delay(50);
+    char c=bluetooth.read();
+    readString+=c;
+  }
+  if(readString.length()>0){
+    Serial.println(readString);
+    if(readString=="FORWARD")
+      front();
+    else if(readString=="BACKWARD")
+      back();
+    else if(readString=="LEFT")
+      left();
+    else if(readString=="RIGHT")
+      right();
+    else if(readString=="STOP")
+      Stop();
+    readString="";
+   }
+}
+
+void front(){
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+}
+
+void back(){
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin4, LOW);
+  digitalWrite(motorPin3, HIGH);
+}
+
+void left(){
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
+  delay(200);
+}
+
+void right(){
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
+  delay(200);
+}
+
+void Stop(){
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
+}
--- Bluetooth_RC_using_Motor_Driver/Bluetooth_RC_using_Motor_Driver.ino
+++ Bluetooth_RC_using_Motor_Driver/Bluetooth_RC_using_Motor_Driver.ino
@@ -1,80 +1,76 @@
-//*** 1- Documentation
-//This program is used to control a robot car using a app that communicates with Arduino trough a bluetooth module
-
+#include <SoftwareSerial.h>
 #include <AFMotor.h>
 
-//creates two objects to control the terminal 3 and 4 of motor shield 
-AF_DCMotor motor1(3); 
-AF_DCMotor motor2(4); 
+AF_DCMotor motor1(1, MOTOR12_8KHZ);
+AF_DCMotor motor2(2, MOTOR12_8KHZ);
 
-char command; 
+int bluetoothTx = A1; 
+int bluetoothRx = A0;
 
-void setup() 
-{       
-  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+String readString;
+
+void setup(){
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  
+  motor1.setSpeed(60);
+  motor2.setSpeed(60);
+  
+  motor1.run (RELEASE);
+  motor2.run (RELEASE);
 }
 
 void loop(){
-  if(Serial.available() > 0){ 
-    command = Serial.read(); 
-    Stop(); //initialize with motors stoped
-    //Change pin mode only if new command is different from previous.   
-    //Serial.println(command);
-    switch(command){
-    case 'F':  
+  while(bluetooth.available()){
+    delay(50);
+    char c=bluetooth.read();
+    readString+=c;
+  }
+  if(readString.length()>0){
+    Serial.println(readString);
+    if (readString =="FORWARD")
       forward();
-      break;
-    case 'B':  
-       back();
-      break;
-    case 'L':  
+    if (readString =="BACKWARD")
+      back();
+    if (readString =="LEFT")
       left();
-      break;
-    case 'R':
+    if (readString =="RIGHT")
       right();
-      break;
-    }
-  } 
+    if (readString =="STOP")
+      Stop();
+    readString="";
+  }
 }
 
-void forward()
-{
+void forward(){
   motor1.setSpeed(255); //Define maximum velocity
   motor1.run(FORWARD); //rotate the motor clockwise
   motor2.setSpeed(255); //Define maximum velocity
   motor2.run(FORWARD); //rotate the motor clockwise
 }
-
-void back()
-{
+void back(){
   motor1.setSpeed(255); 
   motor1.run(BACKWARD); //rotate the motor counterclockwise
   motor2.setSpeed(255); 
   motor2.run(BACKWARD); //rotate the motor counterclockwise
 }
-
-void left()
-{
+void left(){
   motor1.setSpeed(255); //Define maximum velocity
   motor1.run(FORWARD); //rotate the motor clockwise
   motor2.setSpeed(0);
   motor2.run(RELEASE); //turn motor2 off
 }
-
-void right()
-{
+void right(){
   motor1.setSpeed(0);
   motor1.run(RELEASE); //turn motor1 off
   motor2.setSpeed(255); //Define maximum velocity
   motor2.run(FORWARD); //rotate the motor clockwise
 }
-
-void Stop()
-{
+void Stop(){
   motor1.setSpeed(0);
   motor2.run(RELEASE); //turn motor1 off
   motor2.setSpeed(0);
   motor2.run(RELEASE); //turn motor2 off
 }
-
-
