b044e8da67acfecd99ec2f3131c150ea8b86fd61
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2021-10-19 02:22:58 +0530

    v3

--- RFID_Door_Lock.ino
+++ RFID_Door_Lock.ino
@@ -3,17 +3,17 @@
 #include <LiquidCrystal_I2C.h>
 #include <MFRC522.h>
 #include <Wire.h>
-#include <SPI.h>
-#include <DS3231.h>
  
+
+
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 
-LiquidCrystal_I2C lcd(0x3F , 2,1,0,4,5,6,7,3,POSITIVE);
+LiquidCrystal_I2C lcd(0x27 , 2,1,0,4,5,6,7,3,POSITIVE);
 
-int lr=2;
-int lg=3;
+int lr=6;
+int lg=7;
 
 const byte ROWS = 4;
 const byte COLS = 4;
@@ -24,7 +24,7 @@ char hexaKeys[ROWS][COLS] = {
   {'*','0','#','D'}
 };
 byte rowPins[ROWS] = {A0, A1, A2, A3}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6, 5};
+byte colPins[COLS] = {5, 4, 3, 2};
 Keypad myKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 Servo m;
@@ -32,24 +32,33 @@ String password ="1234"; //create a password
 String s;
 String c="";
 String p="1234";
-DS3231  rtc(SDA, SCL);
+//RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup() {
  pinMode(lr,OUTPUT);
  pinMode(lg,OUTPUT);
  lcd.begin(20, 4);
- rtc.begin();
- SPI.begin(); 
+ lcd.setCursor(0,0);
+ lcd.print("Enter Password");
  Serial.begin(9600);
-// lcd.setCursor(0,0);
-// lcd.print("Enter Password");
- m.attach(4);
- mfrc522.PCD_Init();
- /*
-  * rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
-  rtc.setTime(11, 11, 0);     // Set the time to 12:00:00 (24hr format)
-  rtc.setDate(6, 3, 2019);
-  */
+  m.attach(8);
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();
+ /* if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+  
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+     //rtc.adjust(DateTime(2019, 2, 12, 23, 19, 50));
+  }*/
   digitalWrite(lr,1);
   delay(200);
   digitalWrite(lr,0);
@@ -63,34 +72,24 @@ void setup() {
 
 void loop() {
   rfid();
- timed();
- lcd.setCursor(0,2);
- lcd.print("Enter Key ");
- lcd.setCursor(0,3);
- lcd.print(s);
- //lcd.setCursor(0,1);
- //lcd.print("Time: ");
- //lcd.print(rtc.getTimeStr());
- 
- //lcd.setCursor(0,3);
- //lcd.print("Date: ");
-// lcd.print(rtc.getDateStr());
   char b = myKeypad.getKey();
   if(b){
     if(b != '#')
     {
-        s= s+ b;          
+        lcd.setCursor(0,1);
+        s= s+ b;   
+        Serial.println(s);
+        lcd.print(s); 
     }
     else
     {
-      Serial.println(s);
+      Serial.println("En="+s);
       check();
       s="";
     }
   }
-  
- delay(1000);
- lcd.clear();
+  //Time();
+  //Serial.println();
 }
 
 void check()
@@ -103,6 +102,7 @@ void check()
   {
     access(2);
   }
+  
 }
 
 void sweep()
@@ -134,26 +134,26 @@ void rfid()
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-    // Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     //Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      //Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   content.toUpperCase();
-  Serial.print(content.substring(1)+" "); //prints the RFID Tag
+  Serial.println(content.substring(1)+" "); //prints the RFID Tag
   if (content.substring(1) == "0D 11 57 73")
   {
- //  timed();
+//    Time();
     Serial.print(content.substring(1)+" ");
     access(1);
     delay(2000);
+    Serial.println();
     content="";
   }
   else
   {
     access(2);
   }
-  //Serial.println();
 }
 
 void access(int a)
@@ -175,20 +175,42 @@ void access(int a)
   }
   delay(1000);
   lcd.clear();
-  timed();
+  lcd.setCursor(0,0);
   digitalWrite(lr,0);
   digitalWrite(lg,0);
+ //lcd.print("Enter Password");
 }
 
-void timed()
+/*void Time()
 {
-  lcd.setCursor(0,0);
- lcd.print("Time: ");
- lcd.print(rtc.getTimeStr());
-// Serial.print(lcd.print(rtc.getTimeStr()));
- lcd.setCursor(0,1);
- lcd.print("Date: ");
- lcd.print(" ");
- lcd.print(rtc.getDateStr());
- Serial.print(rtc.getDateStr());
-}
+  DateTime now = rtc.now();
+  Serial.print(now.day(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.year(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  int t=(now.hour(), DEC);
+  if(t>12  && t<=24)
+  {
+   Serial.print(t-12);
+  }
+  else
+  {
+   Serial.print(t);
+  }
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  if((now.hour(), DEC)>12)
+  {
+    Serial.print(" am");
+  }
+  else
+  {
+    Serial.print(" pm");
+  }
+}*/
