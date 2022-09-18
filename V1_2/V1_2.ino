783b2b5b5536fe70a5d915d03226e8922d2a0a31
Soumyadeep Shome <43705072+soumya-shome@users.noreply.github.com>
2022-09-18 19:21:37 +0530

    V1_2 3

--- V1_2/V1_2.ino
+++ V1_2/V1_2.ino
@@ -9,8 +9,8 @@ int b=A1;
 void setup() {
   pinMode(a,INPUT);
   pinMode(b,INPUT);
-  motor1.setSpeed(130);
-  motor2.setSpeed(130);
+  motor1.setSpeed(45);
+  motor2.setSpeed(45);
   motor1.run(RELEASE);
   motor2.run(RELEASE);
   Serial.begin(9600);
@@ -19,14 +19,24 @@ void setup() {
 void loop() {
   int c=digitalRead(a);
   int d=digitalRead(b);
-  Serial.print("a"+a);
-  Serial.println("b="+b);
-  if(c==d) {
+  //Serial.print(c);
+  //Serial.println(d);
+  if(c==1 && d==1) {
     motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    Serial.println("Moving");
+  }
+  else if(c==0 && d==1) {
+    motor1.run(FORWARD);
+    motor2.run(RELEASE);
+  }
+  else if(c==1 && d==0) {
+    motor1.run(RELEASE);
     motor2.run(FORWARD);
   }
   else {
     motor1.run(RELEASE);
     motor2.run(RELEASE);
+    Serial.println("Stop");
   }
 }
