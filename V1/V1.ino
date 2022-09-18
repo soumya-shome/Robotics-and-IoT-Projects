c5375822488e685c7bd738eda53f34a2178f141a
Soumyadeep Shome <43705072+soumya-shome@users.noreply.github.com>
2022-09-18 18:22:02 +0530

    V1 2

--- V1/V1.ino
+++ V1/V1.ino
@@ -1,39 +1,94 @@
+#define lc 0
+#define sc 1
 
-int l1=2;
-int l2=3;
-
-int r1=4;
-int r2=5;
-
-int en1=A0;
-int en2=A1;
+#define lm1 3
+#define lm2 5
+#define rm1 9
+#define rm2 10
 
 void setup() {
-  pinMode(r1,OUTPUT);
-  pinMode(r2,OUTPUT);
-  pinMode(l1,OUTPUT);
-  pinMode(l2,OUTPUT);
-  pinMode(en1,OUTPUT);
-  pinMode(en2,OUTPUT);
-  
-  Serial.begin(9600);
+  pinMode(lm1,OUTPUT);
+  pinMode(lm2,OUTPUT);
+  pinMode(rm1,OUTPUT);
+  pinMode(rm2,OUTPUT);
+}
+
+void straight()
+{
+  analogWrite(lm1,110);
+  analogWrite(lm2,0);
+  analogWrite(rm1,110);
+  analogWrite(rm2,0);
+}
+void left_turn1()
+{
+  analogWrite(lm1,0);
+  analogWrite(lm2,90);
+  analogWrite(rm1,140);
+  analogWrite(rm2,0);
+}
+void right_turn1()
+{
+  analogWrite(lm1,140);
+  analogWrite(lm2,0);
+  analogWrite(rm1,0);
+  analogWrite(rm2,90);
+}
+void left_turn2()
+{
+  analogWrite(lm1,0);
+  analogWrite(lm2,80);
+  analogWrite(rm1,160);
+  analogWrite(rm2,0);
+}
+void right_turn2()
+{
+  analogWrite(lm1,160);
+  analogWrite(lm2,0);
+  analogWrite(rm1,0);
+  analogWrite(rm2,80);
+}
+void left_turn3()
+{
+  analogWrite(lm1,0);
+  analogWrite(lm2,40);
+  analogWrite(rm1,180);
+  analogWrite(rm2,0);
+}
+void right_turn3()
+{
+  analogWrite(lm1,180);
+  analogWrite(lm2,0);
+  analogWrite(rm1,0);
+  analogWrite(rm2,40);
+} 
+void left_90()
+{
+  analogWrite(lm1,0);
+  analogWrite(lm2,200);
+  analogWrite(rm1,200);
+  analogWrite(rm2,0);
+}
+void right_90()
+{
+  analogWrite(lm1,200);
+  analogWrite(lm2,0);
+  analogWrite(rm1,0);
+  analogWrite(rm2,200);
+}
+void brake()
+{
+  analogWrite(lm1,255);
+  analogWrite(lm2,255);
+  analogWrite(rm1,255);
+  analogWrite(rm2,255);
 }
 
 void loop() {
-  for(int i=0;i<=255;i++)
-  {
-    analogWrite(en1,i);
-    analogWrite(en2,i);
-
-    digitalWrite(r1,1);
-    digitalWrite(r2,0);
-
-    digitalWrite(l1,1);
-    digitalWrite(l2,0);
-    
-    Serial.println(i);
-    
-    delay(100);
-  }
-  delay(2000);
+  left_90();
+  delay(250);
+  straight();
+  delay(250);
+  brake();
+  delay(3000);
 }
