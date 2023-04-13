28b7765318d430d16e7d9c46cda4c1a3fbb5455c
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-04-13 12:09:51 +0530

    Version 1.1
    The User can connect to the network without editting the code

--- IoT_Digital_Clock.ino
+++ IoT_Digital_Clock.ino
@@ -1,23 +1,27 @@
-#include <NTPClient.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+#include <NTPClient.h>
 #include <Arduino.h>
 #include <TM1637Display.h>
-
+#include <WiFiUdp.h>
+//#define TRIGGER_PIN 4
 #define CLK 2
 #define DIO 0
-
-const char *ssid     = "Soumyadeep";
-const char *password = "10967bubu";
-const long utcOffsetInSeconds = 19800;
+WiFiManager wifiM;
 WiFiUDP ntpUDP;
+const long utcOffsetInSeconds = 19800;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 TM1637Display display(CLK, DIO);
 
-void setup(){
+void setup() {
   Serial.begin(9600);
-  WiFi.begin(ssid, password);
+  Serial.println("\n Starting");
+  wifiM.autoConnect("Techie99","password");
+  Serial.println("connected...yeey :)");
+  //pinMode(TRIGGER_PIN, INPUT);
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
     Serial.print ( "." );
@@ -26,19 +30,21 @@ void setup(){
   display.setBrightness(0x0f);
 }
 
+
 void loop() {
+  /*if ( digitalRead(TRIGGER_PIN) == LOW ) {
+    if (!wifiM.startConfigPortal("Techie99","password")) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      ESP.reset();
+      delay(5000);
+    }
+    Serial.println("connected...yeey :)");
+  }*/
   timeClient.update();
   int times=0;
   times=times+(timeClient.getHours()*100);
-  Serial.print(timeClient.getHours());
-  Serial.print(":");
-  Serial.println(timeClient.getMinutes());
-  //Serial.print(":");
   times=(timeClient.getHours()*100)+timeClient.getMinutes();
   Serial.println(times);
-  //Serial.println(timeClient.getFormattedTime());
   display.showNumberDecEx(times, 0b11100000, true, 4, 0);
-  //delay(50);
-  //display.showNumberDec(times, true);
-  //delay(50);
 }
