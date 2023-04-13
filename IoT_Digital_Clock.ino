4be1d08c4fe806e3b729d6777b287ccfa22b46a7
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-04-13 12:08:27 +0530

    Version 1

--- IoT_Digital_Clock.ino
+++ IoT_Digital_Clock.ino
@@ -1,31 +1,44 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 #include <Arduino.h>
 #include <TM1637Display.h>
 
-// Module connection pins (Digital Pins)
 #define CLK 2
-#define DIO 3
+#define DIO 0
 
-// The amount of time (in milliseconds) between tests
-#define TEST_DELAY   2000
+const char *ssid     = "Soumyadeep";
+const char *password = "10967bubu";
+const long utcOffsetInSeconds = 19800;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 TM1637Display display(CLK, DIO);
 
-void setup()
-{
-}
-int times=1234;
-void loop()
-{
-  
-  //uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-  //uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
+void setup(){
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  timeClient.begin();
   display.setBrightness(0x0f);
-  //display.clear();
-  //delay(TEST_DELAY);
-  display.showNumberDecEx(times, 0b11100000, false, 4, 0);
-  delay(TEST_DELAY);
-  times=times+1;
-//  display.setSegments(SEG_DONE);
-  //delay(TEST_DELAY);
-  //while(1);
+}
+
+void loop() {
+  timeClient.update();
+  int times=0;
+  times=times+(timeClient.getHours()*100);
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.println(timeClient.getMinutes());
+  //Serial.print(":");
+  times=(timeClient.getHours()*100)+timeClient.getMinutes();
+  Serial.println(times);
+  //Serial.println(timeClient.getFormattedTime());
+  display.showNumberDecEx(times, 0b11100000, true, 4, 0);
+  //delay(50);
+  //display.showNumberDec(times, true);
+  //delay(50);
 }
