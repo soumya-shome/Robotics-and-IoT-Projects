dcb81b97d161ec6abb89088b2fad5d0fdbf8b462
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-05-19 23:38:43 +0530

    Version 1.1

--- Bluetooth_Notice_Board.ino
+++ Bluetooth_Notice_Board.ino
@@ -1,31 +1,35 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-String inData;
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
+String readString;
 
 void setup() {
-  Serial.begin(9600);       
-    lcd.begin(16, 2);
-    lcd.print("Welcome!");
-    delay(3000);
-    lcd.clear();
+lcd.init();
+lcd.backlight();
+Serial.begin(9600);
+lcd.setCursor(0,0);
 }
-void loop() {
- int i=0;
-  char commandbuffer[100];
 
-  if(Serial.available()){
+void loop()
+{
+  int i=0;
+  char commandbuffer[100];
+  if(Serial.available())
+  {
      delay(100);
-     while( Serial.available() && i< 99) {
+     while( Serial.available() && i< 99) 
+     {
         commandbuffer[i++] = Serial.read();
      }
      commandbuffer[i++]='\0';
   }
-
   if(i>0)
-
      Serial.println((char*)commandbuffer);
      lcd.print((char*)commandbuffer);
      delay(1000);
      lcd.clear();
 }
+
 
